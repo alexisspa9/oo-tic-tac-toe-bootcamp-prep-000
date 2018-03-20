74c72988e3a2a110707f5e99915ab096955a240d
Alexander Spanos <alexisspa9@gmail.com>
2018-03-20 18:17:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,6 +14,9 @@ class TicTacToe
     ]
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   end
 
 end
